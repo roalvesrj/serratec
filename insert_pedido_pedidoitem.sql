86008b126004bd43bebb740763e091006a26b169
Rômulo Alves <romuloalves.rj@gmail.com>
2020-07-25 08:43:02 -0300

    Versão 25/07 08:42

--- insert_pedido_pedidoitem.sql
+++ insert_pedido_pedidoitem.sql
@@ -10,7 +10,7 @@ INSERT INTO pedidoitem (id_pedido, id_produto, quantidade)
 VALUES
 ((SELECT currval(pg_get_serial_sequence('pedido', 'id_pedido'))), 2, 3);
 
--- Subtrai do estoque a quantidade do item 1
+-- Atualiza estoque do produto do item 1
 UPDATE produto 
 SET quant_estoque = (quant_estoque - (SELECT quantidade FROM pedidoitem WHERE id_pedidoitem = (SELECT currval(pg_get_serial_sequence('pedidoitem', 'id_pedidoitem'))))) 
 WHERE id_produto = (SELECT id_produto FROM pedidoitem WHERE id_pedidoitem = (SELECT currval(pg_get_serial_sequence('pedidoitem', 'id_pedidoitem'))));
@@ -20,7 +20,7 @@ INSERT INTO pedidoitem (id_pedido, id_produto, quantidade)
 VALUES
 ((SELECT currval(pg_get_serial_sequence('pedido', 'id_pedido'))), 1, 1);
 
--- Subtrai do estoque a quantidade do item 2
+-- Atualiza estoque do produto do item 2
 UPDATE produto 
 SET quant_estoque = (quant_estoque - (SELECT quantidade FROM pedidoitem WHERE id_pedidoitem = (SELECT currval(pg_get_serial_sequence('pedidoitem', 'id_pedidoitem'))))) 
 WHERE id_produto = (SELECT id_produto FROM pedidoitem WHERE id_pedidoitem = (SELECT currval(pg_get_serial_sequence('pedidoitem', 'id_pedidoitem'))));
