5d7859d9c6b9ef5a66275b35d8c53b4085ea6646
Gabriel Maiworm <tuxomaiworm@hotmail.com>
2020-07-29 08:11:41 -0300

    novas categorias

--- insert_categoria.sql
+++ insert_categoria.sql
@@ -9,5 +9,13 @@ VALUES
 (4, 'Eletrodomésticos', 'Elétroeletrônicos'),
 (5, 'Móveis', 'Móveis de Casa e Escritório');
 
+INSERT INTO categoria (id_usuario ,nome, descricao)
+values
+(3,'Automóveis','Veículos para Deslocamento'),
+(3,'Utilitarios','Objetos domésticos'),
+(3,'Tapetes','Tapetes para Casas'),
+(3,'Armas','Armas de AirSoft'),
+(3,'Lâmpadas','Lâmpadas Internas e Externas');
+
 
 COMMIT;
